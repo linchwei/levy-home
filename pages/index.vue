2e98314559c277fba91ee12efae3e7ead6d0d521
linchwei <levy0802@qq.com>
2020-08-25 15:48:27 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'levy 站点'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  background: grey;
+}
+</style>
+
